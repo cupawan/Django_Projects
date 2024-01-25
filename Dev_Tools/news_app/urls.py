9292f450dddd01a1e9079d18ad04dc5c6214f90c
Pawan <pavan11896@gmail.com>
2024-01-26 00:02:48 +0530

    Deleted Folder

--- Dev_Tools/news_app/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('bhaskar/', views.dainik_bhaskar, name='dainik_bhaskar'),
-    # ... other URL patterns ...
-]
--- Dev_Tools/translate_app/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('translate/', views.translate_text, name='translate_text'),
-    # ... other URL patterns ...
-]
